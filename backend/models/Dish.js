a11cf4081694f0aa37b136a94b0917cba59523fb
D K VIJENDRA KUMAR <dkvijendra04@gmail.com>
2025-11-22 22:03:23 +0530

    Add files via upload

---
+++ backend/models/Dish.js
@@ -0,0 +1,11 @@
+// backend/models/Dish.js
+const mongoose = require('mongoose');
+
+const dishSchema = new mongoose.Schema({
+    dishId: { type: Number, required: true, unique: true },
+    dishName: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    isPublished: { type: Boolean, required: true, default: true }
+});
+
+module.exports = mongoose.model('Dish', dishSchema);
